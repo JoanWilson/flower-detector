40d4571efa51a6a74f9abd176d87da2a4769022a
Joan Wilson <joanwmoliveira@gmail.com>
2023-05-10 09:13:33 -0300

    Initial Commit

---
+++ FlowerDetector/FlowerDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerDetectorApp.swift
+//  FlowerDetector
+//
+//  Created by Joan Wilson Oliveira on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FlowerDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
